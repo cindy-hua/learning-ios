bf2332a0d788e09105dfdd22f81dd87fb710ccbd
m.cindyhua@gmail.com <m.cindyhua@gmail.com>
2025-01-07 13:14:12 +0100

    Add .gitignore file

---
+++ 100DaysOfSwiftUI/Project5_WordScramble/Project5_WordScramble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project5_WordScramble
+//
+//  Created by HUA Cindy on 07/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
