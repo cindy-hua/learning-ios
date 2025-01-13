a55fe5b58817576c22eb000875d2efb492c158a9
m.cindyhua@gmail.com <m.cindyhua@gmail.com>
2025-01-13 15:39:58 +0100

    Save local changes before merging

---
+++ 100DaysOfSwiftUI/Project9_Navigation/Project9_Navigation/Project9_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project9_NavigationApp.swift
+//  Project9_Navigation
+//
+//  Created by HUA Cindy on 13/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Project9_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
