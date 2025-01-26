0334d03eae888ebd624978c9e37ef1259f8420f1
m.cindyhua@gmail.com <m.cindyhua@gmail.com>
2025-01-26 17:11:19 +0100

    New project

---
+++ 100DaysOfSwiftUI/Milestone10-12_InternetInformation/Milestone10-12_InternetInformation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Milestone10-12_InternetInformation
+//
+//  Created by HUA Cindy on 26/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
