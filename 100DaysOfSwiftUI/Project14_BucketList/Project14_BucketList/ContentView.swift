3c4940b36c8bcafe2e7badefe77a0c2c198d75cd
m.cindyhua@gmail.com <m.cindyhua@gmail.com>
2025-02-09 22:00:47 +0100

    Add Project14 BucketList

---
+++ 100DaysOfSwiftUI/Project14_BucketList/Project14_BucketList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project14_BucketList
+//
+//  Created by HUA Cindy on 09/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100DaysOfSwiftUI/Project14_BucketList/Project14_BucketList/Project14_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project14_BucketListApp.swift
+//  Project14_BucketList
+//
+//  Created by HUA Cindy on 09/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Project14_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
