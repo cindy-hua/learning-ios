369425f26e791df2716a92d551c6605c1971dc23
m.cindyhua@gmail.com <m.cindyhua@gmail.com>
2025-02-19 20:28:30 +0100

    Make views near the top of the scroll view fade out to 0 opacity

---
+++ 100DaysOfSwiftUI/Project18_LayoutAndGeometry/Project18_LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  Project18_LayoutAndGeometry
+//
+//  Created by HUA Cindy on 19/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        let minY = proxy.frame(in: .global).minY
+                        let fadeStart = 200.0
+                        let opacity = min(1, max(0, (minY - fadeStart) / 200))
+                        
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(opacity)
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
